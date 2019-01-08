efd527da5ad78ade67813de7acedfa8bb4969902
joostvi <30979803+joostvi@users.noreply.github.com>
2019-01-08 23:18:41 +0100

    Adding ConsoleLogger

---
+++ GenericClassLibrary/Logging/ConsoleLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenericClassLibrary.Logging
+{
+    public class ConsoleLogger : ILogger
+    {
+
+        public void Debug(string value)
+        {
+            Console.WriteLine("DEBUG: " + value);
+        }
+
+        public void Error(string value)
+        {
+            Console.WriteLine("ERROR: " + value);
+        }
+
+        public void Info(string value)
+        {
+            Console.WriteLine("INFO: " + value);
+        }
+
+        public void Warning(string value)
+        {
+            Console.WriteLine("WARNING: " + value);
+        }
+    }
+}
