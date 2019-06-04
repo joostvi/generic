375c6c0c07a95d0aa4f6a26ff98775373feee12b
joostvi <30979803+joostvi@users.noreply.github.com>
2019-06-04 21:19:09 +0200

    Add ymal helper + update Moq library

---
+++ GenericClassLibrary/Yaml/yamlserializer.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+
+/// <summary>
+/// https://github.com/aaubry/YamlDotNet/wiki/Samples.SerializeObjectGraph
+/// </summary>
+namespace GenericClassLibrary.Yaml
+{
+    public static class YamlSerializer
+    {
+        public static T Deserialize<T>(string fileName) where T : class
+        {
+            using (StringReader input = new StringReader(fileName))
+            {
+                IDeserializer deserializer = new DeserializerBuilder()
+                    .WithNamingConvention(new CamelCaseNamingConvention())
+                    .Build();
+
+                T obj = deserializer.Deserialize<T>(input);
+                return obj;
+            }
+        }
+
+        public static string Serialize<T>(T obj, bool emitDefaults = false) where T : class
+        {
+            SerializerBuilder serializerBuilder = new SerializerBuilder().WithNamingConvention(new CamelCaseNamingConvention());
+            if (emitDefaults)
+            {
+                serializerBuilder.EmitDefaults();
+            }
+            ISerializer serializer = serializerBuilder.Build();
+            return serializer.Serialize(obj);
+        }
+    }
+}
