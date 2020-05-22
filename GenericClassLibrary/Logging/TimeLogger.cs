1b83020f10a7c8ae2bb9502f7b68a86a92c5af2f
joostvi <30979803+joostvi@users.noreply.github.com>
2020-05-22 21:32:40 +0200

    Added logger for timings

---
+++ GenericClassLibrary/Logging/TimeLogger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace GenericClassLibrary.Logging
+{
+    public static class TimeLogger
+    {
+        private static readonly object _lockObject = new object();
+        private static readonly Dictionary<string, Stopwatch> Stopwatches = new Dictionary<string, Stopwatch>();
+
+        public static void Start(EnumLogLevel logLevel, string action)
+        {
+            if (Logger.Level >= logLevel)
+            {
+                lock (_lockObject)
+                {
+                    if (Stopwatches.ContainsKey(action))
+                    {
+                        throw new Exception($"There is already a timer started for action: {action}");
+                    }
+                    Stopwatch sw = new Stopwatch();
+                    Stopwatches.Add(action, sw);
+                    sw.Start();
+                }
+
+            }
+        }
+
+        public static void Stop(string action)
+        {
+            lock (_lockObject)
+            {
+                if (!Stopwatches.TryGetValue(action, out Stopwatch sw))
+                {
+                    return;
+                }
+
+                sw.Stop();
+                Logger.Info($"{action} took {sw.ElapsedMilliseconds} ms.");
+                Stopwatches.Remove(action);
+            }
+        }
+    }
+}
