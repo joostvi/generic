26b613fa1d2dd189369c7772e1e1397eb1c7fdd9
joostvi <30979803+joostvi@users.noreply.github.com>
2020-10-10 19:03:17 +0200

    StringExtensions

---
+++ GenericClassLibrary/Extensions/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericClassLibrary.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ReplaceSpaceByUnderscore(this string input)
+        {
+            return input.Replace(" ", "_");
+        }
+    }
+}
