5d3f273d0250aaa9acf6caef892b5fbb2da157aa
joostvi <30979803+joostvi@users.noreply.github.com>
2020-10-10 20:55:20 +0200

    More StringExtensions

--- GenericClassLibrary/Extensions/StringExtensions.cs
+++ GenericClassLibrary/Extensions/StringExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace GenericClassLibrary.Extensions
 {
@@ -9,6 +10,34 @@ namespace GenericClassLibrary.Extensions
         public static string ReplaceSpaceByUnderscore(this string input)
         {
             return input.Replace(" ", "_");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="aLine"></param>
+        /// <param name="noTrime">When true do not remove trailing spaces</param>
+        /// <returns></returns>
+        public static string ReplaceDuplicateSpace(this string aLine, bool noTrime)
+        {
+            if (noTrime)
+            {
+                ///https://medium.com/factory-mind/regex-tutorial-a-simple-cheatsheet-by-examples-649dc1c3f285
+                /// \b represents an anchor like caret (it is similar to $ and ^) matching positions where one side is a word character (like \w) and the other side is not a word character (for instance it may be the beginning of the string or a space character).
+                /// \s = space
+                /// {2,} = 2 or more times.
+                return Regex.Replace(aLine, "\\b\\s{2,}\\b", " ");
+            }
+            return ReplaceDuplicateSpace(aLine);
+        }
+
+        public static string ReplaceDuplicateSpace(this string aLine)
+        {
+            ///https://medium.com/factory-mind/regex-tutorial-a-simple-cheatsheet-by-examples-649dc1c3f285
+            /// \b represents an anchor like caret (it is similar to $ and ^) matching positions where one side is a word character (like \w) and the other side is not a word character (for instance it may be the beginning of the string or a space character).
+            /// \s = space
+            /// {2,} = 2 or more times.
+            return Regex.Replace(aLine, "\\s{2,}", " ");
         }
     }
 }
---
+++ GenericClassLibraryTests/TestStringExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using GenericClassLibrary.Extensions;
+
+namespace GenericClassLibraryTests
+{
+    public class TestStringExtensions
+    {
+        [Fact]
+        public void ReplaceDuplicateSpace()
+        {
+            string input = "bla  bla";
+            string output = input.ReplaceDuplicateSpace(true);
+
+            Assert.Equal("bla bla", output);
+        }
+
+        [Fact]
+        public void ReplaceDuplicateSpace_NoReplaceAtStart()
+        {
+            string input = "  bla  bla";
+            string output = input.ReplaceDuplicateSpace(true);
+
+            Assert.Equal("  bla bla", output);
+        }
+
+        [Fact]
+        public void ReplaceDuplicateSpace_NoReplaceAtEnd()
+        {
+            string input = "bla  bla  ";
+            string output = input.ReplaceDuplicateSpace(true);
+
+            Assert.Equal("bla bla  ", output);
+        }
+
+        [Fact]
+        public void ReplaceDuplicateSpace_Numbers()
+        {
+            string input = "bla1  2bla";
+            string output = input.ReplaceDuplicateSpace(true);
+
+            Assert.Equal("bla1 2bla", output);
+        }
+
+        [Fact]
+        public void ReplaceDuplicateSpace_ReplaceAtEndAsWell()
+        {
+            string input = "bla  bla  ";
+            string output = input.ReplaceDuplicateSpace(false);
+
+            Assert.Equal("bla bla ", output);
+        }
+
+        [Fact]
+        public void ReplaceDuplicateSpace_ReplaceAtStartAsWell()
+        {
+            string input = "  bla  bla";
+            string output = input.ReplaceDuplicateSpace(false);
+
+            Assert.Equal(" bla bla", output);
+        }
+    }
+}
