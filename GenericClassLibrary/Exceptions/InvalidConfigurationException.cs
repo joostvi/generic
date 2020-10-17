b69caf30a9001d0349607ccdc55de3feec2e2740
joostvi <30979803+joostvi@users.noreply.github.com>
2020-10-17 19:26:05 +0200

    New exception type

---
+++ GenericClassLibrary/Exceptions/InvalidConfigurationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericClassLibrary.Exceptions
+{
+    public class InvalidConfigurationException : Exception
+    {
+        public InvalidConfigurationException()
+        {
+        }
+
+        public InvalidConfigurationException(string message) : base(message)
+        {
+        }
+    }
+}
